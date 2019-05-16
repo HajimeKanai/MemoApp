613ebad618f2986741dbbed1f12a55770c3fb4e5
Hajime Kanai <kanai027peace@gmail.com>
2019-05-16 09:32:22 +0900

    add delete and edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "memos#index"
   get "/new" => "memos#new"
   post "/create" => "memos#create"
+  delete "/memos/:id" => "memos#destroy"
+  get "/memos/:id/edit" => "memos#edit"
+  patch "/memos/:id" => "memos#update"
 end
